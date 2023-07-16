c967a18e6fedeee5371272d105afc0e496042e20
syed mohammed <85601222+syedmohammedgouse@users.noreply.github.com>
2023-07-17 01:15:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,6 +16,629 @@ def load_image(image_file):
 
 
 
+
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+
+st.header("Face Matching Project using AWS")
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+
+
+
+
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+
+st.header("Face Matching Project using AWS")
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+
+
+
+
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+
+st.header("Face Matching Project using AWS")
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+
+
+
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+
+st.header("Face Matching Project using AWS")
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+
+st.header("Face Matching Project using AWS")
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+
+st.header("Face Matching Project using AWS")
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+
+st.header("Face Matching Project using AWS")
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+
+st.header("Face Matching Project using AWS")
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+
+st.header("Face Matching Project using AWS")
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+
+st.header("Face Matching Project using AWS")
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+
+st.header("Face Matching Project using AWS")
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+
+st.header("Face Matching Project using AWS")
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+
+st.header("Face Matching Project using AWS")
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+
+st.header("Face Matching Project using AWS")
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+
+st.header("Face Matching Project using AWS")
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+
+st.header("Face Matching Project using AWS")
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+
+st.header("Face Matching Project using AWS")
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+
+st.header("Face Matching Project using AWS")
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+
+st.header("Face Matching Project using AWS")
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+
+st.header("Face Matching Project using AWS")
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+
+st.header("Face Matching Project using AWS")
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+
+st.header("Face Matching Project using AWS")
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+
+st.header("Face Matching Project using AWS")
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+
+st.header("Face Matching Project using AWS")
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+
+st.header("Face Matching Project using AWS")
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+
+st.header("Face Matching Project using AWS")
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+
+st.header("Face Matching Project using AWS")
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+
+st.header("Face Matching Project using AWS")
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+
+st.header("Face Matching Project using AWS")
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+
+st.header("Face Matching Project using AWS")
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+
+st.header("Face Matching Project using AWS")
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+
+st.header("Face Matching Project using AWS")
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+
+st.header("Face Matching Project using AWS")
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+
+st.header("Face Matching Project using AWS")
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+
+st.header("Face Matching Project using AWS")
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+
+st.header("Face Matching Project using AWS")
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+
+st.header("Face Matching Project using AWS")
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+
+st.header("Face Matching Project using AWS")
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+
+st.header("Face Matching Project using AWS")
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+
+st.header("Face Matching Project using AWS")
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+
+st.header("Face Matching Project using AWS")
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+
+st.header("Face Matching Project using AWS")
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+
+st.header("Face Matching Project using AWS")
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+
+st.header("Face Matching Project using AWS")
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+
+st.header("Face Matching Project using AWS")
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+
+st.header("Face Matching Project using AWS")
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+
+st.header("Face Matching Project using AWS")
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+
+st.header("Face Matching Project using AWS")
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+
+st.header("Face Matching Project using AWS")
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+
+st.header("Face Matching Project using AWS")
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+
+st.header("Face Matching Project using AWS")
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+
+st.header("Face Matching Project using AWS")
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+
+st.header("Face Matching Project using AWS")
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+
+st.header("Face Matching Project using AWS")
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+
+st.header("Face Matching Project using AWS")
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+
+st.header("Face Matching Project using AWS")
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+
+st.header("Face Matching Project using AWS")
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+
+st.header("Face Matching Project using AWS")
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+
+st.header("Face Matching Project using AWS")
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+
+st.header("Face Matching Project using AWS")
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+
+st.header("Face Matching Project using AWS")
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+
+st.header("Face Matching Project using AWS")
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+
+st.header("Face Matching Project using AWS")
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+
+st.header("Face Matching Project using AWS")
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+
+st.header("Face Matching Project using AWS")
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+
+st.header("Face Matching Project using AWS")
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+
+st.header("Face Matching Project using AWS")
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+
+st.header("Face Matching Project using AWS")
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+
+st.header("Face Matching Project using AWS")
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+
+st.header("Face Matching Project using AWS")
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+
+st.header("Face Matching Project using AWS")
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+
+st.header("Face Matching Project using AWS")
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+
+st.header("Face Matching Project using AWS")
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+
+st.header("Face Matching Project using AWS")
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+
+st.header("Face Matching Project using AWS")
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+
+st.header("Face Matching Project using AWS")
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+
+
+
 st.header("Face Matching Project using AWS")
 def load_image(image_file):
     img=Image.open(image_file)
